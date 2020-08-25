a5fc8e664b3bf7ade48fa8219e5641c07907d689
migrepo <70193518+migrepo@users.noreply.github.com>
2020-08-25 02:28:51 -0500

    Update link.js

--- link.js
+++ link.js
@@ -1,4 +1,2 @@
-function toWebinar(){
-    window.open('https://upc.workplace.com/work/landing/input/', '_blank');
-    console.log(linkWebinar);
-}
+var webinarLink ='https://upc.workplace.com/work/landing/input/';
+
